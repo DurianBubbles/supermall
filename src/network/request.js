354591a5adfded6ed39e62ee74be08e4eed6d2f9
DurainBubbles <1791229116@qq.com>
2021-10-09 20:32:28 +0800

    add test.json

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config){
     const instance =  axios.create({
-        baseURL:'http://httpbin.org/',
+        baseURL:'http://localhost:8080/',
         timeout:5000
     })
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,24 +3,15 @@
     <NavBar class="home-nav">
       <div slot="center">购物街</div>
     </NavBar>
-    <img src="https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/140012/8/1804/3641/5efbf318E38bd5dad/0db99d859ab16ce9.png" alt="">
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar.vue'
-import {request} from '@/network/request.js'
 export default {
   name:'Home',
   components:{
     NavBar
-  },
-  created(){
-    request({
-      url:'/headers'
-    }).then(res => {
-      console.log(res)
-    })
   }
 }
 </script>
