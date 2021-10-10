9eaa0db4e70cd36d0bdcea6c69c23d97f3824366
DurainBubbles <1791229116@qq.com>
2021-10-10 16:41:03 +0800

    network package

--- src/network/request.js
+++ src/network/request.js
@@ -5,6 +5,21 @@ export function request(config){
         baseURL:'http://localhost:8080/',
         timeout:5000
     })
+    
+    // 请求拦截器
+    instance.interceptors.request.use(config => {
+        return config
+    },err => {
+        console.log(err)
+    })
 
+    //响应拦截器
+    instance.interceptors.request.use(res => {
+        return res.data
+    },err => {
+        console.log(err)
+    })
+
+    // 发送网络请求
     return instance(config)
 }
