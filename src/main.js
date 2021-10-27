ef4ba31ee87de8f822fe69f636540106d36749cc
DurainBubbles <1791229116@qq.com>
2021-10-27 22:10:45 +0800

    add fastclick

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js'
 import store from './store/index.js'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+// 解决移动端300ms点击事件延迟
+fastclick.attach(document.body)
 
 
 new Vue({
